5a45f1379e53245e2e5f6d6c58d776d58175ae04
Anders Rune Jensen <arj03@protonmail.ch>
2016-03-15 15:07:01 +0100

    Minor fix to export and add import

--- export-data.js
+++ export-data.js
@@ -4,17 +4,24 @@ var fs = require('fs');
 var pull = require('pull-stream');
 var toPull = require('stream-to-pull-stream');
 
-function exportFeed(userId, sbot) {
+function ensureDirExists(exportDir, callback)
+{
+    if (!fs.existsSync(exportDir)) {
+        fs.mkdir(exportDir, function(err){
+            if (err) throw err;
+
+            callback();
+        });
+    } else
+        callback();
+}
+
+function exportFeed(userId, exportDir, sbot) {
     // load data
     pull(sbot.createUserStream({ id: userId }), pull.collect(function (err, log) {
         if (err) throw err;
 
-        var messages = "";
-        log.forEach(function(msg) {
-            messages += JSON.stringify(msg) + "\n";
-        });
-
-        fs.writeFile("messages.txt", messages);
+        fs.writeFile(exportDir + "/messages.txt", JSON.stringify(log));
     }));
 
     var blobs, blobMessageMap = {};
@@ -28,22 +35,24 @@ function exportFeed(userId, sbot) {
             log.forEach(function(msg) {
                 pull(
                     sbot.blobs.get(msg.dest),
-                    toPull.sink(fs.createWriteStream(msg.dest))
+                    toPull.sink(fs.createWriteStream(exportDir + "/" + msg.dest))
                 );
             });
+
+            sbot.close();
         }));
 }
 
-var argv = require('minimist')(process.argv.slice(2));
-var userId = argv._[0];
+var userId = process.argv[2];
+var exportDir = process.argv[3];
 
 if (!require('ssb-ref').isFeed(userId)) {
-    console.error('Usage: export-data.js {feedid}');
+    console.error('Usage: export-data.js --feedid {feedid} {exportDir}');
     process.exit(1);
 }
 
 require('ssb-client')(function (err, sbot) {
     if (err) throw err;
 
-    exportFeed(userId, sbot);
+    ensureDirExists(exportDir, function() { exportFeed(userId, exportDir, sbot); });
 });
---
+++ import-data.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var pull = require('pull-stream');
+var toPull = require('stream-to-pull-stream');
+
+function checkEnd(insertedMessages, sbot)
+{
+    setTimeout(function () {
+        if (insertedMessages.length == 0) {
+            sbot.close();
+            console.log("done importing feed");
+        } else {
+            console.log("not done yet, outstanding: " + insertedMessages.length);
+            checkEnd(insertedMessages, sbot);
+        }
+    }, 100);
+}
+
+function importFeed(userId, exportDir, sbot) {
+    sbot.latestSequence(userId, function (err, seq) {
+        var latestLocalSeq = seq.sequence;
+        console.log("latest seq:" + latestLocalSeq);
+        var messages = JSON.parse(fs.readFileSync(exportDir + "/messages.txt"));
+        var insertedMessages = [];
+        messages.forEach(function(msg) {
+            if (msg['value'].sequence > latestLocalSeq) {
+                console.log("new message with id: " + msg['value'].sequence);
+                insertedMessages.push(msg['value']);
+                // FIXME: blobs
+                sbot.add(msg['value'], function(err) {
+                    if (err) throw err;
+
+                    insertedMessages.pop();
+                });
+            }
+        });
+
+        checkEnd(insertedMessages, sbot);
+    });
+}
+
+var userId = process.argv[2];
+var exportDir = process.argv[3];
+
+if (!require('ssb-ref').isFeed(userId)) {
+    console.error('Usage: import-data.js {feedid} {exportDir}');
+    process.exit(1);
+}
+
+require('ssb-client')(function (err, sbot) {
+    if (err) throw err;
+
+    importFeed(userId, exportDir, sbot);
+});
