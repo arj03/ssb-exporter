160166edef5afb815463617a5b00a780982f21e4
Anders Rune Jensen <arj03@protonmail.ch>
2016-03-22 21:46:15 +0100

    Sync blobs as well

--- sync-mirrors.js
+++ sync-mirrors.js
@@ -5,12 +5,13 @@ var http = require('http');
 var shttp = require('socks5-http-client');
 var url = require('url');
 var path = require('path');
+var ref = require('ssb-ref');
 
 var sanitize = require("sanitize-filename");
 
 var userMirrors = { '@6CAxOI3f+LUOVrbAl0IemqiS7ATpQvr9Mdw9LC4+Uv0=.ed25519': ['http://26thfiwfn3i3wnrf.onion/ssb/messages.txt'] };
 
-function writeFile(filename, res)
+function writeFile(filename, res, cb)
 {
     var f = null;
     var fd = 0;
@@ -32,33 +33,75 @@ function writeFile(filename, res)
         } else {
             var date = new Date(res.headers["last-modified"]);
             fs.futimesSync(fd, date, date);
+
+            if (cb != null)
+                cb();
         }
     });
+}
+
+function syncBlobs(remotePath, feedFile, user, userHttp)
+{
+    var messages = JSON.parse(fs.readFileSync(feedFile));
+    messages.forEach(msg => {
+        if (msg['value'].content.mentions)
+        {
+            msg['value'].content.mentions.forEach(o => {
+                if (ref.type(o.link) != 'blob')
+                    return;
+
+                var blobFilename = sanitize(user) + "/" + sanitize(o.link);
+
+                if (fs.existsSync(blobFilename))
+                    return;
+
+                var options = url.parse(remotePath + "/" + sanitize(o.link));
+                if (remotePath.indexOf(".onion/") != -1)
+                    options.socksPort = 9050; // tor default port
+
+                userHttp.get(options, res => writeFile(blobFilename, res));
+            });
+        }
+    });
+}
+
+function ensureDirExists(exportDir, cb)
+{
+    if (!fs.existsSync(exportDir)) {
+        fs.mkdir(exportDir, function(err){
+            if (err) throw err;
+
+            cb();
+        });
+    } else
+        cb();
 }
 
 for (var user in userMirrors)
 {
     userMirrors[user].forEach(mirror => {
-        var options = url.parse(mirror);
+        ensureDirExists(sanitize(user), () => {
+            var options = url.parse(mirror);
 
-        var filename = path.basename(options.pathname);
+            var filename = sanitize(user) + "/" + path.basename(options.pathname);
 
-        var stats = null;
-        try {
-            stats = fs.statSync(filename);
-            options.headers = {'If-Modified-Since': stats.mtime.toUTCString() };
-        } catch(err) {
-            //console.log("stat error:" + filename + ", error:" + err);
-        }
+            var stats = null;
+            try {
+                stats = fs.statSync(filename);
+                options.headers = {'If-Modified-Since': stats.mtime.toUTCString() };
+            } catch(err) {
+                //console.log("stat error:" + filename + ", error:" + err);
+            }
 
-        if (mirror.indexOf(".onion/") != -1) {
-            options.socksPort = 9050; // tor default port
+            var userHttp = http;
 
-            shttp.get(options, res => writeFile(filename, res));
-        }
-        else
-        {
-            http.get(options, res => writeFile(filename, res));
-        }
+            if (mirror.indexOf(".onion/") != -1)
+            {
+                options.socksPort = 9050; // tor default port
+                userHttp = shttp;
+            }
+
+            userHttp.get(options, res => writeFile(filename, res, () => syncBlobs(path.dirname(mirror), filename, user, userHttp)));
+        });
     });
 }
