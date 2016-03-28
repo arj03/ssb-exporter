847af5f85948d003797113320c9fcb6f450f465d
Anders Rune Jensen <arj03@protonmail.ch>
2016-03-28 17:09:12 +0200

    Add script to sync one mirror

---
+++ sync-mirror.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+var common = require('./lib/common');
+var lib = require('./lib/sync-mirrors');
+
+var program = require('commander');
+
+program
+  .option('-m, --mirror [value]', 'Mirror to sync')
+  .option('-e, --export-dir [value]', 'Dir to export mirror to')
+  .parse(process.argv);
+
+require('ssb-client')((err, sbot) => {
+    if (err) throw err;
+
+    if (program.mirror == "") {
+        console.error('No mirror specified. See --help');
+        process.exit(1);
+    }
+
+    var exportDir = program.exportDir || "";
+
+    if (exportDir != "")
+        common.ensureDirExists(exportDir, function() { lib.syncMirror(program.mirror, exportDir, sbot); });
+    else
+        lib.syncMirror(program.mirror, exportDir, sbot);
+});
